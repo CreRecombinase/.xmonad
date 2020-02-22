7489d4f2208d27258dfa19fe373cc8c8eefb4ebe
Nicholas Knoblauch <nwknoblauch@gmail.com>
2020-02-22 11:51:07 -0600

    working version

--- xmonad.hs
+++ xmonad.hs
@@ -1,16 +1,18 @@
 import XMonad
-import XMonad.Config
 import XMonad.Hooks.DynamicLog
-import XMonad.Util.EZConfig
 import qualified XMonad.StackSet as W
+import Control.Monad
+import XMonad.Util.EZConfig(additionalKeysP)
 
-main = xmonad  defaultConfig
-    {
+main:: IO () -- {{{1
+main = xmonad <=< xmobar $ def {
       modMask = mod4Mask
     , terminal = "urxvt"
     , borderWidth = 3
     }
     `additionalKeysP`
         [ ("M-<Up>", windows W.swapUp)
-        , ("M-c", spawn "emacsclient -c -n -e '(switch-to-buffer nil)'")
+        , ("M-<Down>", windows W.swapDown)
+        , ("M-e", spawn "emacsclient -c -n -e '(switch-to-buffer nil)'")
+        , ("M-f", spawn "firefox")
         ]
